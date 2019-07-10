19ede9eaffb94037b7cfd901f67283c4551c1e8d
Ed Phillips <ed@paperuncle.com>
2019-07-10 09:41:21 +0100

    brand colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brandYellow: '#FFDC00',
+        brandOrange: '#FF851B',
+        brandBlue: '#7FDBFF',
+        brandDBlue: '#0074D9',
+        brandPink: '#F012BE',
+        brandGreen: '#2ECC40'
+      }
+    }
   },
   variants: {}
 }
