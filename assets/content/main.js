e1136e2b0e8bf03ef2aef7c047d35fa9b3c0356e
Ed Phillips <ed@paperuncle.com>
2019-07-10 09:41:02 +0100

    data

---
+++ assets/content/main.js
@@ -0,0 +1,118 @@
+const data = {
+  header: {
+    greeting: 'Catherine Phillips',
+    subtitle:
+      "I'm Catherine Phillips and I'm a Creative Director & Copywriter in International Advertising."
+  },
+  projects: [
+    {
+      title: 'Disneyland',
+      subtitle: 'CRM',
+      slug: 'disneyland-crm',
+      thumb: 'disney_zurg.gif',
+      category: 'crm',
+      description:
+        'Award-winning sequence of over 20 brand emails and landing pages, which boosted click rates into double figures. Immersive stories and game scenarios.'
+    },
+    {
+      title: 'Citroën DS3',
+      subtitle: 'Digital campaign',
+      slug: 'citroen-ds3-digital-campaign',
+      thumb: 'citroen_www.jpg',
+      category: 'www',
+      description:
+        'Imagine you can take one last drive through your city before leaving forever. Collecting personal routes from hundreds of users, we reveal the most memorable parts of cities.'
+    },
+    {
+      title: 'PartyLite',
+      subtitle: 'Rebrand, website, crm',
+      slug: 'partylite-rebrand-website-crm',
+      thumb: 'partylitecover.jpg',
+      category: 'atl',
+      description:
+        'One of the magical times we created the strategy as a team. Resultingly meticulous UX with pristine design and copy, all around the theme of warmth. I presented with the creative director to the PartyLite CEO in Boston.'
+    },
+    {
+      title: 'Intel',
+      subtitle: 'Videos',
+      thumb: 'intelitsjustus_script.gif',
+      category: 'video',
+      description:
+        'Comedy sequences featuring IT dept colleagues, Io and Oll, who continually arrive at an impasse. Made to circulate in the IT crowd, the message: It takes more than 2.'
+    },
+    {
+      title: 'National Blood Service',
+      subtitle: 'Outdoor',
+      thumb: 'blooddonor.png',
+      category: 'atl',
+      description:
+        '"In which a man on the street saves lives by giving blood. Little do passers-by realise that Mr B is a hero in disguise. Just a day in the life of a blood donor."'
+    },
+    {
+      title: 'Piaget',
+      subtitle: 'Luxury branding',
+      thumb: 'piagetcover.jpg',
+      category: 'atl',
+      description:
+        'Collection of jewels inspired by the deserts of the American West. "Endless expanses of silent, ironed heat. Hot air, full of light. Shade is colorful with a scent of heat and leaf."'
+    },
+    {
+      title: 'Nissan',
+      subtitle: 'TVC',
+      thumb: 'nissanqashqai_tvc2.gif',
+      category: 'video',
+      description:
+        'Models Qashqai, Juke and X-Trail are made for adventure, so we told stories about "Leaving what you know behind", erasing the past to reveal something new and unexpected.'
+    },
+    {
+      title: 'Moët Champagne',
+      subtitle: 'Branding and ooh',
+      thumb: 'moet.jpg',
+      category: 'atl',
+      description:
+        'The cork pops. You are in all places, at all times, with everyone. When your glass sparkles, you are in synchrony. You are alive in the Now.'
+    },
+    {
+      title: 'Wonderbra',
+      subtitle: 'Augmented reality website',
+      thumb: 'wonderbra_www.jpg',
+      category: 'www',
+      description:
+        'Where augmented reality previews the bigger boobs you could have. "Ready for more than a push up the ladder of life? Want a boost, a bounce up, a Full Effect fruity, juicy jump up?"'
+    },
+    {
+      title: 'Garnier',
+      subtitle: 'Video and disruptive',
+      thumb: 'garnier_colormatch.jpg',
+      category: 'atl',
+      description:
+        'Video, social, and some clever retargeting banners launch this app that lets you match your hair color to anything… including that thing you just bought on Amazon.'
+    },
+    {
+      title: 'Disneyland',
+      subtitle: 'B2B website',
+      thumb: 'dlp_b2b.gif',
+      category: 'www',
+      description:
+        "Disneyland's 20th-birthday celebration site for press, employees and business partners. Constructed of tiny modules of content, it was fluid and reorganisable depending on use."
+    },
+    {
+      title: 'SkyTeam',
+      subtitle: 'Social',
+      thumb: 'skyteamborder_fbgame.gif',
+      category: 'crm',
+      description:
+        'Do you stress at border control? Activates camera and mic to track markers of nervousness as you face the toughest security guard ever.'
+    },
+    {
+      title: 'Augmented Identity',
+      subtitle: 'Posters',
+      thumb: 'idemia.jpg',
+      category: 'atl',
+      description:
+        'Showcasing the new tech behind the biometric mobile driver’s license, facial recognition for secure transactions, fingerprint voting and more.'
+    }
+  ]
+}
+
+export default data
