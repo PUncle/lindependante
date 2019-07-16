f636771a5f80dfc05ec55788147b4b6f3f261c8f
Ed Phillips <ed@paperuncle.com>
2019-07-16 16:52:36 +0100

    header and footer templates

---
+++ components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer
+    class="min-height w-full flex md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 md:px-2"
+  >
+    <div class="min-w-full bg-brandYellow text-white p-4">
+      <h1 class="text-6xl">GOODBYE</h1>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+  // props: {
+  //   greeting: {
+  //     type: String,
+  //     required: true
+  //   },
+  //   subtitle: {
+  //     type: String,
+  //     required: true
+  //   }
+  // }
+}
+</script>
+
+<style>
+footer {
+  position: relative;
+  z-index: 1;
+  min-height: 275px;
+}
+
+footer::after {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  bottom: 0;
+  width: 99999em;
+  background-color: theme('colors.brandYellow');
+  z-index: -1;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <header class="min-height w-full flex lg:w-2/3 xl:w-1/2 mb-4 md:px-2">
+    <div class="min-w-full bg-brandYellow text-white p-4">
+      <h1 class="text-6xl">{{ header.greeting }}</h1>
+      <p class="text-brandDBlue">
+        {{ header.subtitle }}
+      </p>
+      <p>SOCIAL</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    // header: {
+    //   type: Object,
+    //   required: true,
+    //   greeting: {
+    //     type: String,
+    //     required: true
+    //   }
+    // }
+    // greeting: {
+    //   type: String,
+    //   required: true
+    // },
+    // subtitle: {
+    //   type: String,
+    //   required: true
+    // }
+  },
+  computed: {
+    header() {
+      return this.$store.state.header
+    }
+  }
+}
+</script>
+
+<style>
+header {
+  position: relative;
+  z-index: 2;
+  min-height: 275px;
+}
+
+@media (min-width: 1480px) {
+  header::before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 100%;
+    bottom: 0;
+    left: -99999em;
+    transform: translateX(0.5rem);
+    background-color: theme('colors.brandYellow');
+    background-image: url(~assets/images/hello.svg);
+    background-position: top right;
+    background-repeat: no-repeat;
+    z-index: -1;
+  }
+}
+</style>
