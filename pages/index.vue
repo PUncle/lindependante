043270e6d3428b1b0bb122f37c4bd3ba8b3b7b88
Ed Phillips <ed@paperuncle.com>
2019-07-16 18:03:08 +0100

    index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,57 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        laconceptrice
-      </h1>
-      <h2 class="subtitle">
-        Work by Catherine Phillips, Creative Director and Copywriter
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
-  </div>
+  <main class="wrapper flex mb-4 flex-wrap justify-start mx-auto">
+    <Header />
+    <ProjectCard
+      v-for="project in projects"
+      :key="project.slug"
+      :title="project.title"
+      :subtitle="project.subtitle"
+      :slug="project.slug"
+      :description="project.description"
+      :thumb="project.thumb"
+      :category="project.category"
+    />
+    <Footer />
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Header from '~/components/Header'
+import Footer from '~/components/Footer'
+import ProjectCard from '~/components/ProjectCard'
 
 export default {
   components: {
-    Logo
+    Header,
+    ProjectCard,
+    Footer
+  },
+  data() {
+    return {
+      // ...data
+    }
+  },
+  computed: {
+    projects() {
+      return this.$store.state.projects.projects
+    }
   }
+  // methods: {
+  //   pathify(title) {
+  //     return title
+  //       .normalize('NFD')
+  //       .replace(/[\u0300-\u036f]/g, '')
+  //       .replace(/[^a-z0-9]+/gi, '-')
+  //       .replace(/^-+/, '')
+  //       .replace(/-+$/, '')
+  //       .toLowerCase()
+  //   }
+  // }
 }
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+/* .min-height {
+  min-height: 20rem;
+} */
 </style>
