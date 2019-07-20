bcafaa51ea0652a5ade1b1998282c0510b5517f5
Ed Phillips <ed@paperuncle.com>
2019-07-20 17:55:36 +0100

    better 404

--- pages/_projectDetail/index.vue
+++ pages/_projectDetail/index.vue
@@ -1,10 +1,12 @@
 <template>
   <main class="wrapper flex mb-4 flex-wrap justify-start mx-auto">
-    <Header />
-    <!-- <h1>{{ project.title }}</h1>
-    <p>{{ project.description }}</p> -->
-    <!-- eslint-disable-next-line vue/no-v-html -->
-    <article class="text-white p-4" v-html="content"></article>
+    <Header :wide="true" />
+    <article class="min-height w-full flex mb-4 px-2">
+      <div class="min-w-full bg-white text-grey-800 px-4 py-10">
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <div class="md:w-3/4 xl:w-1/2 m-auto" v-html="content"></div>
+      </div>
+    </article>
     <Footer />
   </main>
 </template>
@@ -26,7 +28,7 @@ export default {
     }
   },
 
-  async asyncData({ params }) {
+  async asyncData({ params, error }) {
     try {
       const fileContent = await import(
         `@/static/projects/${params.projectDetail}/${params.projectDetail}.md`
@@ -35,9 +37,13 @@ export default {
         content: fileContent.default
       }
     } catch (err) {
-      return {
-        content: `<h2>TUTU! There is no Markdown file found for this project!</h2><p>${err}</p>`
-      }
+      error({
+        statusCode: 404,
+        message: 'Oh. It seems that you may be lost.'
+      })
+      // return {
+      //   content: `<h2>TUTU! There is no Markdown file found for this project!</h2><p>${err}</p>`
+      // }
     }
   }
 }
